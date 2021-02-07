fe730497fa9d27f58247ee5ba5a587875d9cea81
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-02-07 14:43:50 -0800

    Player Stats Introduced
    Stamina, Max Stamina, and Sanity in a working state

---
+++ Mirage/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private PlayerStats myStats;
+
+    public float speed;
+    [SerializeField]public float defaultSpeed;
+
+    private void Awake()
+    {
+        defaultSpeed = speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xInput + transform.forward * zInput + -transform.up;
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            myStats.isRunning = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            myStats.isRunning = false;
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        Debug.Log(speed);
+    }
+}
---
+++ Mirage/Assets/Scenes/Scripts/PlayerStats.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public float sprintSpeedMultiplier;
+    public float sanityDepletionRate;
+
+    [SerializeField] private float sprintTimer;
+    [SerializeField] private PlayerMovement movePlayer;
+
+    [HideInInspector] public float sanity;
+    [HideInInspector] public float stamina;
+    [HideInInspector] public float maxStamina;
+    private float minStamina;
+    private float hallucinationTimer;
+
+    private float runSpeed;
+
+    private float timer;
+
+    [HideInInspector] public bool isRunning = false;
+
+    private void Awake()
+    {
+        sanity = 20f;
+       maxStamina = 10f;
+        minStamina = 5f;
+        hallucinationTimer = 120f;
+
+        stamina = maxStamina;
+        runSpeed = movePlayer.speed * sprintSpeedMultiplier;
+
+    }
+    private void Update()
+    {
+        CalculateSanity();
+
+        Sprint();
+
+
+        Debug.Log(stamina);
+    }
+
+    private void CalculateSanity()
+    {
+        sanity -= Time.deltaTime * sanityDepletionRate;
+
+        CalculateMaxStamina();
+    }
+
+    private void CalculateMaxStamina()
+    {
+        maxStamina = sanity / 2;
+
+        if (stamina > maxStamina)
+        {
+            stamina = maxStamina;
+        }
+    }
+
+
+    public void Sprint()
+    {
+        if (isRunning)
+        {
+            movePlayer.speed = runSpeed;
+
+            stamina -= Time.deltaTime;
+            if (stamina < 0)
+            {
+                stamina = 0;
+                isRunning = false;
+                movePlayer.speed = movePlayer.defaultSpeed;
+            }
+        }
+
+        else if (stamina < maxStamina)
+        {
+            movePlayer.speed = movePlayer.defaultSpeed;
+            stamina += Time.deltaTime / 2f;
+        }
+
+        else
+        {
+            movePlayer.speed = movePlayer.defaultSpeed;
+        }
+    }
+
+    
+    public void ResetSprint(float originalSpeed)
+    {
+        movePlayer.speed = originalSpeed;
+    }
+}
+
