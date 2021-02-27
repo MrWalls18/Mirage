29cf2b9ae64705d1f859e1633ef56b981e5681af
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-02-27 13:13:26 -0800

    CleanedUp Project Folders a bit and added Terrain Hallucination

---
+++ Mirage/Assets/Scripts/Hallucinations/SwitchingTerrain.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchingTerrain : MonoBehaviour
+{
+    [SerializeField] private GameObject realTerrain;
+    [SerializeField] private GameObject fakeTerrain;
+    private MeshRenderer realRenderer, fakeRenderer;
+
+    private PlayerStats myStats;
+
+
+    private void Awake()
+    {
+        myStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+        realRenderer = realTerrain.GetComponent<MeshRenderer>();
+        fakeRenderer = fakeTerrain.GetComponent<MeshRenderer>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!realRenderer.isVisible
+            && !fakeRenderer.isVisible)
+        {
+            if (myStats.isHallucinating)
+            {
+                realTerrain.SetActive(false);
+                fakeTerrain.SetActive(true);
+            }
+            else
+            {
+                fakeTerrain.SetActive(false);
+                realTerrain.SetActive(true);
+            }
+        }
+    }
+}
