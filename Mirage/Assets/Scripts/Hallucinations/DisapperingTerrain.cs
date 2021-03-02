e39bfdb1459e70a93556969d70586aa6184b0e6f
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-03-02 10:18:08 -0800

    Added Terrain Fading

---
+++ Mirage/Assets/Scripts/Hallucinations/DisapperingTerrain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisapperingTerrain : MonoBehaviour
+{
+    [SerializeField] private GameObject terrain, checkForVisibility;
+    private MeshRenderer terrainRenderer, visibilityRenderer;
+
+    private PlayerStats myStats;
+
+    private void Awake()
+    {
+        myStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+        terrainRenderer = terrain.GetComponent<MeshRenderer>();
+        visibilityRenderer = checkForVisibility.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!terrainRenderer.isVisible
+            && !visibilityRenderer.isVisible)
+        {
+            if (myStats.isHallucinating)
+                terrain.SetActive(true);
+            else
+                terrain.SetActive(false);
+        }
+        
+    }
+}
