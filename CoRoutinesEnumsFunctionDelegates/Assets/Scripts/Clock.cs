bff069a5afea3cd853c8c4544d25a7783c6b7bac
KatatoniaNightIsTheNewDay <78169460+KatatoniaNightIsTheNewDay@users.noreply.github.com>
2021-03-10 22:58:28 -0800

    Level 1 and 2 changes

---
+++ CoRoutinesEnumsFunctionDelegates/Assets/Scripts/Clock.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Tick());
+        StartCoroutine(DoCheck());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //bool warnflag = ProximityCheck();
+    }
+
+    /*bool ProximityCheck()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if(Vector3.Distance(transform.position, enemies[i].transform.position) < dangerDist)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+   */     
+
+
+
+    IEnumerator DoCheck()
+    {
+        for (; ; )
+        {
+            //ProximityCheck();
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    //IEnumerator is a return type for all coroutine classes
+    //stands for InterfaceEnumerator
+    IEnumerator Tick()
+    {
+        //need an infinite while loop to keep the print happening
+        //until corouting is halted or program is stopped
+        while (true)
+        {
+            print(System.DateTime.Now.ToString());
+
+            //this yield statement tell the corouting to wait 1 second
+            //before continuing
+            //Note - coroutine time isn't exact
+            yield return new WaitForSeconds(1f);
+        }
+
+
+    }
+
+}
---
+++ CoRoutinesEnumsFunctionDelegates/Assets/Scripts/Pet.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//enums are declared outside of a class definition
+/// <summary>
+/// Enums are actually ints masquerading as other values
+/// which means they can be cast to ints
+/// </summary>
+
+public enum PetType
+{
+    none,
+    dog,
+    cat,
+    bird,
+    fish,
+    other
+}
+
+public enum Gender
+{
+    unspecified,
+    female,
+    male
+}
+
+public class Pet : MonoBehaviour
+{
+    public string name = "Flash";
+    public PetType pType = PetType.dog;
+    public Gender gender = Gender.male;
+
+    private void Awake()
+    {
+        int i = (int)PetType.cat; //i is = 2
+        PetType pt = (PetType)4; //pt equals PetType.fish
+
+        switch (pType)
+        {
+            case PetType.none:
+                break;
+            case PetType.dog:
+                break;
+            case PetType.cat:
+                break;
+            case PetType.bird:
+                break;
+            case PetType.fish:
+                break;
+            case PetType.other:
+                break;
+            default:
+                break;
+        }
+
+        switch (gender)
+        {
+            case Gender.unspecified:
+                break;
+            case Gender.female:
+                break;
+            case Gender.male:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+
+}
