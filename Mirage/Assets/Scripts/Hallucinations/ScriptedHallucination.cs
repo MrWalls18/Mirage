3d149ba0d6803201db40b55dd3c2954dd524f8bf
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-03-30 19:40:59 -0700

    Changed prefabs

--- Mirage/Assets/Scripts/Hallucinations/ScriptedHallucination.cs
+++ Mirage/Assets/Scripts/Hallucinations/ScriptedHallucination.cs
@@ -8,18 +8,9 @@ public class ScriptedHallucination : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("TriggerEntered");
         if (other.tag == "Player")
         {
             scriptedEnemies.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            boulderBlockade.SetActive(false);
         }
     }
 }
