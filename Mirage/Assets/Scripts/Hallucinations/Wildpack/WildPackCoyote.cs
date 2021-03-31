df47611f48201c5991ff4e7403f6482bb7848b0e
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-03-30 20:09:08 -0700

    made temp wildpack

---
+++ Mirage/Assets/Scripts/Hallucinations/Wildpack/WildPackCoyote.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WildPackCoyote : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent coyoteAgent;
+
+    [SerializeField] private Transform wildpackDestination;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coyoteAgent.SetDestination(wildpackDestination.position);
+        
+    }
+}
