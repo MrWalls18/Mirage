b9489acaac759f2192317f5b91e95a242036a6cb
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-04-13 12:23:12 -0700

    changes

---
+++ Mirage/Assets/Scripts/Hallucinations/CaveIn/CaveIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveIn : MonoBehaviour
+{
+    [SerializeField] private GameObject rockPrefab, boulderPrefab;
+    private GameObject rockClone, boulderClone;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(SpawnRocks());
+        }
+    }
+
+    IEnumerator SpawnRocks()
+    {
+        rockClone = Instantiate(rockPrefab, transform.position, transform.rotation);
+        yield return new WaitForSeconds(0.5f);
+    }
+
+}
