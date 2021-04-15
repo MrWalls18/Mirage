66a70e38178f8705ff6c20f881d226467b86d667
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-04-15 07:44:40 -0700

    Edited playerStats

--- Mirage/Assets/Scripts/Player/PlayerStats.cs
+++ Mirage/Assets/Scripts/Player/PlayerStats.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.PackageManager.UI;
+//using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
