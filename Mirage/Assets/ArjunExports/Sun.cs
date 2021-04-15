7394856f4b12b8aa7ba5b17632ac0806282c904d
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-04-15 03:36:04 -0700

    Imported Assets

---
+++ Mirage/Assets/ArjunExports/Sun.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    [SerializeField] private float transformSpeed = 1f;
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, transformSpeed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
