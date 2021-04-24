78dcd55fe549fb955167ff4e8df5b1a6fde30bfd
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-04-24 12:51:36 -0700

    Created CoyoteLooming and Added to gameSystem prefab

--- Mirage/Assets/Scripts/Hallucinations/CoyoteLooming/CoyoteLooming.cs
+++ Mirage/Assets/Scripts/Hallucinations/CoyoteLooming/CoyoteLooming.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CoyoteLooming : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] SamplePostion getCoyotePosition;
+    public float rangeToSpawn;
+    public Transform player;
+    [SerializeField] private GameObject fakeEnemyPrefab;
+    private GameObject fakeEnemyClone;
+
+    private bool isSpawned = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 coyotePosition;
+
+        if(!isSpawned && getCoyotePosition.RandomPoint(player.position, rangeToSpawn, out coyotePosition))
+        {
+            if (Vector3.Distance(player.position, coyotePosition) > rangeToSpawn - 5)
+            {
+                fakeEnemyClone = Instantiate(fakeEnemyPrefab, coyotePosition, transform.rotation);
+
+                isSpawned = true;
+                this.gameObject.SetActive(false);
+            }
+        }
     }
 }
