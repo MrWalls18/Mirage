6d6024d97a193f211741e4585b3263d5e93eaae8
Arjun-Gambhir <70241653+Arjun-Gambhir@users.noreply.github.com>
2021-05-02 21:57:42 -0700

    UI pass part 1, Sun fix
    UI Prefabs

--- Mirage/Assets/ArjunExports/Sun.cs
+++ Mirage/Assets/ArjunExports/Sun.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Sun : MonoBehaviour
+public class Sun : SingletonPattern<Sun>
 {
+    [Header("Rotates at 1 degree per second, set to 6 for 1 minute. Divide this number by the amount multiplied for the amount of time you want it to take")]
     [SerializeField] private float transformSpeed = 1f;
+    public float timer = 0f;
+    public float sunMultiplier = 1.0f;
+    public float sunPercentage;
+    [SerializeField] private float daylightAngle = 240f;
 
-    void Update()
+    private void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, transformSpeed * Time.deltaTime);
-        transform.LookAt(Vector3.zero);
+        MoveSunlight(sunMultiplier);
     }
+
+    public void MoveSunlight(float multiplier)
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, transformSpeed * sunMultiplier * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+        timer += (Time.deltaTime * sunMultiplier);
+        sunPercentage = (timer * transformSpeed) / daylightAngle;
+    }
+
+
 }
---
+++ Mirage/Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class InventoryItem
+{
+    public bool isUnlocked;
+    public GameObject item;
+    public Sprite inventorySprite;
+}
+
+public class InventoryUI : MonoBehaviour
+{
+    public List<InventoryItem> inventory = new List<InventoryItem>();
+    public int index;
+    public GameObject equippedItem;
+    [SerializeField] private float panelScale = 1.4f;
+    public List<GameObject> panels = new List<GameObject>();
+
+    private void Start()
+    {
+        UpdateItemPanels();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            setIndex(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            setIndex(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            setIndex(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            setIndex(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            setIndex(4);
+        }
+    }
+
+    public void changeIndex(int amountToChange)
+    {
+        index += amountToChange;
+        if (index >= inventory.Count)
+        {
+            index = 0;
+        }
+        if (index < 0)
+        {
+            index = inventory.Count - 1;
+        }
+        equippedItem = inventory[index].item;
+        UpdateItemPanels();
+    }
+
+    public void setIndex(int indexToSet)
+    {
+        index = indexToSet;
+        if (index >= inventory.Count)
+        {
+            index = 0;
+        }
+        if (index < 0)
+        {
+            index = inventory.Count - 1;
+        }
+        equippedItem = inventory[index].item;
+        UpdateItemPanels();
+    }
+
+    public void useItem()
+    {
+
+    }
+
+    public void UpdateItemPanels()
+    {
+        for (int i = 0; i < panels.Count; i++)
+        {
+            if (i != index)
+            {
+                panels[i].transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            }
+            else
+            {
+                panels[i].transform.localScale = new Vector3(panelScale, panelScale, 1.0f);
+            }
+        }
+    }
+
+    
+
+}
