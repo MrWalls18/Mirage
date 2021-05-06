8dd12cffacdf4045e15b87b28927125d3ecdb8eb
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-05-06 02:42:40 -0700

    Created Howl Hallucination, issues with audio manager

---
+++ Mirage/Assets/HowlHallucination.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowlHallucination : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered Howl");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Should play sound");
+            AudioManager.instance.Play("Coyote_howl_day");
+        }
+    }
+}
