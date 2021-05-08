8d4fa059aa0f1ce49a4486ab5a00bc5dab6cf124
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-05-08 16:19:40 -0700

    UPdated enemy size

--- Mirage/Assets/HowlHallucination.cs
+++ Mirage/Assets/HowlHallucination.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class HowlHallucination : MonoBehaviour
 {
+    private bool playedHallucination = false;
+
     [SerializeField] AudioSource coyoteHowl;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entered Howl");
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !playedHallucination)
         {
             Debug.Log("Should play sound");
             AudioManager.instance.Play("Coyote_howl_day");
+            playedHallucination = true;
            // coyoteHowl.Play();
         }
     }
