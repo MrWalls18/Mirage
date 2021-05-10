8e6d4e4b28735d954565912a28b346783805e41c
JoeWarren95 <jlwarren@mail.csuchico.edu>
2021-05-10 16:32:43 -0700

    Coyote animation modifications

---
+++ Mirage/Assets/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    //private Animator animator;
+    // Start is called before the first frame update
+    public void EndGame()
+    {
+        //animator = GetComponent<Animator>();
+        //animator.SetBool("isPlayerInMinAttackRange", true);
+        //player dies, trigger death screen
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
--- Mirage/Assets/Scripts/Player/SkillCheckTimer.cs
+++ Mirage/Assets/Scripts/Player/SkillCheckTimer.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SkillCheckTimer : MonoBehaviour
+public class SkillCheckTimer : SingletonPattern<SkillCheckTimer>
 {
     public static SkillCheckTimer s_instance;
 
