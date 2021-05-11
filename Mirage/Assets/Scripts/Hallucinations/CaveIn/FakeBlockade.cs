1b918d01d0098eb006332758d0cdd5a4cd131517
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-05-11 00:26:38 -0700

    Fixed Cavein and added more spawn points

--- Mirage/Assets/Scripts/Hallucinations/CaveIn/FakeBlockade.cs
+++ Mirage/Assets/Scripts/Hallucinations/CaveIn/FakeBlockade.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class FakeBlockade : MonoBehaviour
 {
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(true);
+            this.gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !this.CompareTag("EntryCaveIn"))
         {
             this.gameObject.SetActive(false);
         }
     }
+
 }
