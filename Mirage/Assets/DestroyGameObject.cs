1c8025a12daa8a2710fcef201c890da2465b2144
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-05-10 19:50:19 -0700

    Updated CoyotesSpawns and Jumpscare

---
+++ Mirage/Assets/DestroyGameObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGameObject : MonoBehaviour
+{
+    IEnumerator TimeToDestroy()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+
+
+    private void Start()
+    {
+        TimeToDestroy();
+    }
+}
